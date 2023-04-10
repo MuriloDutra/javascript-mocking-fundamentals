1eb1ccf680fc5194f022d6adfb49c9a239e6998e
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2023-04-10 20:05:08 -0300

    Mocking getWinner function

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -2,11 +2,16 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+//Saving the original value of the function
 const originalGetWinner = utils.getWinner
-utils.getWinner = (p1, p2) => p1
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+//Mocking the function
+utils.getWinner = (p1, p2) => p2
 
-// cleanup
+const winner = thumbWar('Murilo Alves', 'Camila Pereira')
+assert.strictEqual(winner, 'Camila Pereira')
+
+//Retrieving the original function, this is important because
+//can be other tests that doesn't want to use the mocked function, but
+//the original one
 utils.getWinner = originalGetWinner
